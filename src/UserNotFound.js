1e58b5bf42648b3df5aec82b5f5b0d061d32e780
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-08-28 22:12:45 +0430

    add UserNotFound component

---
+++ src/UserNotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserNotFound() {
+  return (
+    <section className='user-not-found'>
+      <h1>User not found!</h1>
+    </section>
+  )
+}
+
+export default UserNotFound
