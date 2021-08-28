097edb85a290316f0f53d4e9a317fe7b8327dd52
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-08-28 22:13:00 +0430

    add NoFollowers component

---
+++ src/NoFollowers.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NoFollowers() {
+  return (
+    <div className='no-followers'>
+      <h1>The user doesn't have any followers</h1>
+    </div>
+  )
+}
+
+export default NoFollowers
